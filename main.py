0c1722e556fb27a3702a508299376c4590969405
Scott Morales <morales.scott@gmail.com>
2020-09-30 22:18:52 -0500

    Refactoring and Renaming (#1)
    * Working on loading first package
    
    * Rename CSVReader to readCSVFile. Move readCSVFile to tools file.

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 # Robert S Morales 000954923
 
-import csvReader
 import hashTable
 import tools
 
 packageFile = 'WGUPS Package File.csv'
 distanceTable = 'WGUPS Distance Table.csv'
 
-dataObject = csvReader.csvReader(packageFile) # This loads the data csv file into a hash object
-distanceObject = csvReader.csvReader(distanceTable, True) # This loads the distance csv file into a hash object
+dataObject = tools.readCSVFile(packageFile) # This loads the data csv file into a hash object
+distanceObject = tools.readCSVFile(distanceTable, True) # This loads the distance csv file into a hash object
 truck1 = []
 #truck1.append()
 
