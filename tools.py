b005371037a441d9ce00be556e56b92a7ad3544a
scottmo223 <morales.scott@gmail.com>
2020-09-30 22:00:35 -0500

    Rename CSVReader to readCSVFile. Move readCSVFile to tools file.

--- tools.py
+++ tools.py
@@ -1,5 +1,24 @@
 # Robert S Morales 000954923
 
+import csv
+import hashTable
+
+def readCSVFile(file, makeArray = False):
+    with open(file, newline='') as csvfile:
+        csvReader = csv.reader(csvfile, delimiter=',')
+        
+        if makeArray == True:
+            newObject = []
+            for row in csvReader:
+                newObject.append(row)
+            return newObject
+        else:
+            newObject = hashTable.HashTable()
+            next(csvReader) #this grabs the header row and gets it out of the way
+            for row in csvReader:
+                newObject.addItem(row[0],row)
+            return newObject
+
 def findShortestDistance(options): # I had (location, options) with the intent of having the address relevant somehow
     shortestDistance = float(options[1])
     shortestDistanceIndex = 1
