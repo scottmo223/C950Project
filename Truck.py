3695b8c245bf5680f50be5dc96bb5a8417220d94
scottmo223 <morales.scott@gmail.com>
2020-10-01 23:50:10 -0500

    Add set and get functions to class and update truck capacity

--- Truck.py
+++ Truck.py
@@ -2,10 +2,22 @@
 # Truck Class
 
 class Truck:
-    def __init__(self, truckId, packages, capacity = 14):
+    def __init__(self, truckId, capacity = 16):
         """
         Initialized truck class.
         """
         self.truckId = truckId
-        self.packages = packages
+        self.packages = []
         self.truckCapacity = capacity
+
+    def loadPackage(self, packageId):
+        """
+        Add a package to the delivery queue.
+        """
+        self.packages.append(packageId)
+
+    def deliverPackage(self):
+        """
+        Removes a package from the front of the queue and returns the package key.
+        """
+        return self.packages.pop(0)
