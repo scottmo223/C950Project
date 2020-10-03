5df10da813441265a0b6613d0830d48ab1fbf14b
scottmo223 <morales.scott@gmail.com>
2020-10-03 16:07:02 -0500

    Testing changes all successful

--- main.py
+++ main.py
@@ -9,17 +9,16 @@ distanceTable = 'WGUPS Distance Table.csv'
 
 packageHashTable = tools.readCSVFile(packageFile) # This loads the data csv file into a hash object
 distanceObject = tools.readCSVFile(distanceTable, True) # This loads the distance csv file into a hash object
-packagesInHub = [i[0] for i in packageHashTable.keyAddressMap]
 
 #LETS START LOADING TRUCK 1
-truck1 = Truck.Truck(1, packagesInHub)
-firstPackageIds = packageHashTable.getItemsByAddress(tools.initialAddress(distanceObject))
-truck1.loadPackages(firstPackageIds, packageHashTable)
-print('I loaded your first package for you.')
-print('Truck1 packages: ', truck1.packagesOnTruck)
-print('Packages at hub: ', packageHashTable.packagesAtHub)
-print('Packages on trucks: ', packageHashTable.packagesOnTruck)
-print('Packages delivered: ', packageHashTable.packagesDelivered)
+# truck1 = Truck.Truck(1)
+# firstPackageIds = packageHashTable.getItemsByAddress(tools.initialAddress(distanceObject))
+# truck1.loadPackages(firstPackageIds, packageHashTable)
+# print('I loaded your first package for you.')
+# print('Truck1 packages: ', truck1.packagesOnTruck)
+# print('Packages at hub: ', packageHashTable.packagesAtHub)
+# print('Packages on trucks: ', packageHashTable.packagesOnTruck)
+# print('Packages delivered: ', packageHashTable.packagesDelivered)
 
 # print('\nNow it\'s your turn. These are the packages left to load: ')
 # print(truck1.packagesLeftToLoad)
@@ -43,15 +42,20 @@ print('Packages delivered: ', packageHashTable.packagesDelivered)
 #THIS WORKS - PRINTS THE ADDRESS OF THE INITIAL PACKAGE, SHORTEST OR LONGEST
 # shortestAddress = tools.initialAddress(distanceObject)
 # longestAddress = tools.initialAddress(distanceObject,False)
-# print(packageHashTable.getItemsByAddress(shortestAddress))
-# print(packageHashTable.getItemsByAddress(longestAddress))
+# print('Address of shortest initial address: ',(shortestAddress))
+# print('Address of longest initial address: ',(longestAddress))
 
 #THIS WORKS - PRINTS THE STATUS OF A PACKAGE (uses the variables shortestAddress and longestAddress above)
 # print(tools.checkPackageStatus(packageHashTable, packageHashTable.getItemsByAddress(longestAddress)[0]))
 
 #NOTES TO SELF
-# print(distanceObject) #2 dim array - array[searches down][searches across]
-# print(distanceObject[4][0]) #This gives the ADDRESS of the 4th row
+# print(distanceObject.addressDistanceMatrix) #2 dim array - array[searches down][searches across]
+# print(distanceObject.addressDistanceMatrix[4][0]) #This gives the ADDRESS of the 4th row
+# distanceObject.addressVisited(4)
+# print('\nvisitied address 4')
+# print('address not visited: ',distanceObject.addressesNotVisited)
+# print('address visited',distanceObject.addressesVisited)
+# print('index/address map',distanceObject.indexAddressMap)
 
 #THIS WORKS - PRINTS THE INDEX OF THE SHORTEST (SMALLEST NUMBER) IN AN ARRAY
 # print(distanceObject[5])
