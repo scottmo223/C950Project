9ee338237edb91fe829437f786f97daf1bbd2f6a
scottmo223 <morales.scott@gmail.com>
2020-10-02 23:17:51 -0500

    Add a key to address map for easier address lookup

--- hashTable.py
+++ hashTable.py
@@ -6,15 +6,16 @@ class HashTable():
     A Custom hash table. The default length is 1000, but
     this can be changed for future growth.
     """
-
     def __init__(self, length=1000):
         self.array = [None] * length
+        self.keyAddressMap = []
 
     def addItem(self, data):
         index = self.getIndex(data.packageId)
         if self.array[index] is None:
             self.array[index] = []
-        self.array[index].append(data)        
+        self.array[index].append(data)
+        self.keyAddressMap.append([data.packageId,data.deliveryAddress])
 
     def getItem(self, key):
         index = self.getIndex(key)
@@ -24,12 +25,15 @@ class HashTable():
             if i.packageId == key:
                 return i
 
-    def getItemByAddress(self, address):
-        matchingItems = []
-        for i in self.array:
-            if i is not None and i[0].deliveryAddress == address:
-                matchingItems.append(i[0].packageId)
-        return matchingItems
+    def getItemsByAddress(self, address):
+        """
+        Returns a list of package keys matching the given address.
+        """
+        matchingAddresses = []
+        for i in self.keyAddressMap:
+            if i[1] == address:
+                matchingAddresses.append(i[0])
+        return matchingAddresses
 
     #indexing an int just gives the int - should pass it in as a string.
     def getIndex(self, key): 
